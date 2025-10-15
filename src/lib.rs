ee6d855cd013c6530d68f96fb31f71e8d3a2e4ec
Dhruvin Gandhi <contact@dhru.vin>
2025-10-15 21:30:49 +0530

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 Dhruvin Gandhi <contact@dhru.vin>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
